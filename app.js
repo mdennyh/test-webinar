fa6fec802fcf5599fcc52cd8d4eb2891931ad2e5
denny <mdennyh@live.com>
2014-08-01 16:15:32 +0700

    Init project first !!!!

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.configure(function(){
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
 });
 
 app.configure('production', function(){
